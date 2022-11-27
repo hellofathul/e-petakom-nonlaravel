ca0c21013f2c9f0a5a0bbe43b2ad64b4f4b9d558
hellofathul <116142085+hellofathul@users.noreply.github.com>
2022-11-28 00:19:24 +0800

    Adjust toggle function

--- module_fathul/main.js
+++ module_fathul/main.js
@@ -17,8 +17,8 @@ const themeBtn = document.querySelector('.theme-btn');
 
 themeBtn.addEventListener('click', () => {
     document.body.classList.toggle('dark-theme');
-    themeBtn.querySelector('span:first-child').classList.toggle('active');
-    themeBtn.querySelector('span:last-child').classList.toggle('active');
+    themeBtn.querySelector('span:first-child').classList.toggle('active-theme');
+    themeBtn.querySelector('span:last-child').classList.toggle('active-theme');
 })
 
 
--- module_fathul/sidebar_toggle.js
+++ module_fathul/sidebar_toggle.js
@@ -3,13 +3,12 @@ $(function () {
     $('a[href*="#"]').on('click', function (e) {
         e.preventDefault()
         oldObjChild = $('.sidebar > a'); //gets active nav-item child nav-link
-        oldObj = $('.active'); //gets the active nav-item
-        oldObj.removeClass('active'); //remove active from old nav-item
-        oldObjChild.css('background', 'transparent'); //clear old active nav-item and nav-link style for bg color
-        $(this).addClass('active'); //set the active class on the nav-item that called the function
+        oldObj = $('.active-sidebar-btn'); //gets the active nav-item
+        oldObj.removeClass('active-sidebar-btn'); //remove active from old nav-item
+        // oldObjChild.css('background', 'transparent'); //clear old active nav-item and nav-link style for bg color
+        $(this).addClass('active-sidebar-btn'); //set the active class on the nav-item that called the function
         // $(this).css('color', '#86848c'); //set the color
         // $(this).css('border-radius', '1rem'); //set the border radius
-        $(this).css('background', 'white');
         // $('head').append('<style>.sidebar a.active:before{content:"";width:20px;height:100%;position:absolute;background:#3c37ff;border-radius: 1rem 0rem 0rem 1rem;}</style>');
     });
 });
